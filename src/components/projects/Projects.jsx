a11ce56db02330603023a5ef03fc5fbd752021d2
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-08-04 00:39:25 -0500

    lots more css glass effect on cards

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,17 +6,19 @@ const Projects = () => {
   return (
     <>
       <h1 className="pgTitle">...CREATE & INSPIRE</h1>
-      <div className="projects">
-        {portfolioData.map((item, idx) => (
-          <div className="itemCard" key={idx}>
-            <img className="itemImg" src={item.img} alt="" />
-            <div className="itemTitle">{item.title}</div>
-            <div>
-              <ProjCardNav item={item} />
+     
+        <div className="projects">
+          {portfolioData.map((item, idx) => (
+            <div className="itemCard" key={idx}>
+              <img className="itemImg" src={item.img} alt="" />
+              <div className="itemTitle">{item.title}</div>
+              <div>
+                <ProjCardNav item={item} />
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+     
     </>
   );
 };
